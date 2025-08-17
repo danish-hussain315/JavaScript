2cd7d7fabee2b2119a201b83427d9b4416fe1bfd
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-17 13:03:36 +0000

    Variable covered

---
+++ JS_Basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 34;
+let emailID ="danish@gmaiil.com"
+var accountPassword="sddfkjj"
+accountCity = "Lahore"
+console.log(accountPassword);
+
+accountCity= "Karachi"
+
+console.table([accountId , emailID, accountPassword, accountCity])
+
+/*
+We can not updade the value of the variable declaired with const key word;
+var was used in when no concept of the block Scope in JS
+dont declare without key word
+*/
