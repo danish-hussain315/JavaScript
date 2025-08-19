fda252c19a5e9da5be321396ead53815214e31fa
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-19 18:54:15 +0000

    62 tasks done

--- JS_Basics/Objects.js
+++ JS_Basics/Objects.js
@@ -6,3 +6,24 @@ let my_obj = {name: "Ali", age: 25}
 console.log(my_obj.age);
 console.log(my_obj.name);
 
+//task 58 Access property with bracket notation.
+console.log(my_obj["name"]);
+
+
+//task 59 Add a new property to object.
+my_obj.experience = "2 years"
+console.log(my_obj.experience);
+
+
+// task 60 Delete a property.
+// delete my_obj.name
+console.log(my_obj);
+
+// task 61 Check if a property exists (in).
+console.log("age" in my_obj);
+
+
+//task 62 Use Object.keys(obj).
+
+let keys = Object.keys(my_obj)
+console.log(keys);
