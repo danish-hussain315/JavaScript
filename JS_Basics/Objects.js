4b106480503d1eb844b137ce8ea5edb49eca07c7
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-19 19:56:59 +0000

    3 tasks are remaininng

--- JS_Basics/Objects.js
+++ JS_Basics/Objects.js
@@ -37,3 +37,60 @@ console.log(valuess);
 //task 64 Use Object.entries(obj).
 let entry = Object.entries(my_obj)
 console.log(entry);
+
+
+//task 65 Loop through object with for...in.
+for(key in my_obj){
+  console.log(`the value of ${key +` is `+ my_obj[key]}`);
+  
+}
+
+// task 66 Clone object using spread {...obj}.
+
+let newMyObject = {...my_obj}
+console.log(newMyObject);
+
+
+// task 67 Nested object access.
+let newObject = {
+  name: "Danish Hussain",
+  age : 25,
+  address : {
+    colony : "Al Syed colony",
+    near : "DMC Office",
+    road : "Badh Road"
+  }
+}
+
+console.log(newObject.address.road);
+
+
+// task 68 Freeze an object (Object.freeze).
+Object.freeze(my_obj);
+console.log(Object.isFrozen(my_obj))
+
+
+//task 69 Seal an object (Object.seal).
+Object.seal(my_obj)
+
+
+
+//task 70 Merge two objects with Object.assign.
+let mergedObject =  Object.assign({}, my_obj , newMyObject)
+console.log(mergedObject);
+
+
+
+//task 71 Check object type using typeof.
+console.log(typeof my_obj);
+
+
+//task 72 Create an array of objects (students).
+let students = [{name : "Danish "}, {name : "Hussain"}]
+
+
+
+//task 73 Find object by property in array.
+
+
+//task 74
