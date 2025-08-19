94cf8394b6fa734d2693810670db1fa7e97d1cd4
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-19 18:34:04 +0000

    Objects Practice Started

---
+++ JS_Basics/Objects.js
@@ -0,0 +1,8 @@
+
+// task 56 Create an object {name: "Ali", age: 25}. 
+let my_obj = {name: "Ali", age: 25}
+
+//task 57 Access object property with dot notation.
+console.log(my_obj.age);
+console.log(my_obj.name);
+
