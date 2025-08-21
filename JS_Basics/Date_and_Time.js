16d60dd5ba25b723e4ff00fc966abac8b60006d1
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-21 00:36:57 +0000

    Data and Time Demo

---
+++ JS_Basics/Date_and_Time.js
@@ -0,0 +1,16 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+
+//Creating Date Object and applying the methods on it
+let myPersonalPracticeDate = new Date(2001, 10, 12)
+console.log(myPersonalPracticeDate.toLocaleDateString);
+
+
+
+// creating timeStamp
+console.log("   /n/n/n/n/n/n\n\n\n\n\n\n");
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(mydate.getTime())
