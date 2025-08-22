a074d1dec6e0b81f4c2256d9f1589385281ab863
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-22 05:25:32 +0000

    Basic Scope Demo Completed

---
+++ JS_Basics/Function_Lexical_Scope.js
@@ -0,0 +1,18 @@
+let a =10
+const b = 20
+var c =30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+let i =  10;
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+  
+  
+}
+
+console.log(i);
+
+
