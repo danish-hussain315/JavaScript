304cb498dc6ce95dfddaf674d0990caa88e72eaa
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-23 16:28:41 +0000

    while and do while loop

---
+++ JS_Basics/loops.js
@@ -0,0 +1,15 @@
+let count = 0;
+while (count <= 10) {
+  console.log(`Value of the Count ${count}`);
+  count = count +2
+
+}
+
+
+let array = ["Danish" , "hussain ", "Mangi"]
+let index = 0
+while (index < array.length) {
+  console.log(array[index]);
+  index++
+  
+}
