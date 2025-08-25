d72af470843a5fd07a35637c85c3ddc3c877d521
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-25 09:57:00 +0000

    Functions

--- Personal Practice Tasks/function.js
+++ Personal Practice Tasks/function.js
@@ -1,13 +1,82 @@
-//76 I will Perform the fuc
+// ++++++++++++++++++++++++++++++++++ 76 I will Perform the function task with +++++++++++++++++++ 
 
-
+/*
 // Write a function to add two numbers.                                 
 function add (val1 , val2 ){
   return val1 + val2
 }
-
+console.log(add(5,4))
 
 let sumOfTwoNumbersArrowFuntion =  (val1 , val2) =>  (val1 + val2)
 let sumOfTwoNumbersArrowFuntionWithReturn =  (val1 , val2) => { return(val1 + val2)}
 
 console.log(sumOfTwoNumbersArrowFuntion(3,10));
+*/
+
+
+
+
+
+
+
+
+
+/// ++++++++++++++++ Write a function with default parameter. ++++++++++++
+/*function functionWithDefaultParamters(name = "Danish"){
+  return name
+}
+
+console.log(functionWithDefaultParamters("Hussain"));
+
+
+const arrowFunctionWithDefaultParamters = (name ="Dnish") =>  name;
+console.log(arrowFunctionWithDefaultParamters(1));
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//++++++++++++++++ Write a function expression. +++++++++++++++++++++++++++
+let functionExpressiong = function printExpression(){
+  console.log(`this is printed from the Functional Expression`);
+  return ""
+}
+console.log(functionExpressiong());
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//++++++++++++++++++++++++  Write an arrow function. +++++
+const printThroughTheArrowFunction = () => {return "Printed From Arrow Function"}
+const printThroughTheArrowFunctionWithourReturn = () =>  "Printed From Arrow Function Without Return"
+console.log(printThroughTheArrowFunction());
+console.log(printThroughTheArrowFunctionWithourReturn());
+
