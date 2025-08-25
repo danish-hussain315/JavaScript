d23681de2510235b2918c23d69037960bae6fa35
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-25 09:12:54 +0000

    Higher Order function

---
+++ JS_Basics/for-loop.js
@@ -0,0 +1,4 @@
+for (let j = 0; j < array.length; j++) {
+  const element = array[j];
+  
+}
---
+++ JS_Basics/hof.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const name = numbers.forEach((arrayValue) => {
+    console.log(arrayValue +3);
+    // return "Danish"
+})                      
+
+
+console.log(name);
+
+
