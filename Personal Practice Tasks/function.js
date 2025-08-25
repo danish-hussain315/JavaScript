a03e5a8fbc6b2cf78e6e687da648d8aec571113a
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-25 10:41:45 +0000

    83 tasks completed

--- Personal Practice Tasks/function.js
+++ Personal Practice Tasks/function.js
@@ -85,7 +85,7 @@ console.log(printThroughTheArrowFunctionWithourReturn());
 
 
 
-
+/*
 // ++++++++++++++++++ Return multiple values from function (object/array). +++++++++++++++
 // let returmArrayFromFuntion = function retunsFrom(){
 //   return ["danish", "Hussain", "Mangi"]
@@ -95,3 +95,69 @@ let returmArrayFromFuntion = function retunsFrom(){
   return  {"array" : ["danish", "Hussain", "Mangi"]}
 }
 console.log(returmArrayFromFuntion().array[0]);
+
+*/
+
+
+
+
+
+
+/* 
+// ++++++++++++++++++  Write a function to check even/odd. ++++++++
+function checkEvenOrOdd(number){
+  if(number % 2 ==0){
+    return "Number is Even"
+  }else {
+    return "Number is Odd"
+  }
+}
+
+console.log(checkEvenOrOdd(1)); */
+
+
+
+
+
+/* 
+// ++++++++++++++++++++ Function to find factorial of a number. +++++++
+
+function factorial(num){
+  let fact = 1;
+  for (let i = 1;  i<= num; i++) {
+    fact =fact*i
+  }
+  return fact;
+}
+
+function findFactorial(number){
+  if (number <0) {
+    return "Cannot find Factorial of the Negative Number "
+  } else if(number == 0) {
+    return `Factorial of 0 is 1`
+  }else{
+    return factorial(number)
+  }
+}
+console.log(findFactorial(4)); */
+
+
+
+
+
+
+/* 
+//++++++++++++++++ Function to reverse a string. ++++++++
+function reverseStrings(name = "Danish"){
+  return name.split("").reverse().join()
+}
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString("hussain")); // niassuh
+console.log(reverseStrings()); */
