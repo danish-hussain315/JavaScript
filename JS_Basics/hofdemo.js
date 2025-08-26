6a4da44e2aacbbbc629943233254ecc98d9d20d3
Danish Hussain <148607096+danish-hussain315@users.noreply.github.com>
2025-08-26 10:02:05 +0000

    array loops

---
+++ JS_Basics/hofdemo.js
@@ -0,0 +1,14 @@
+let arr = [3, 4, 5, 6, 4]
+
+// Using for of Loop on array
+for (const element of arr) {
+  console.log(element);
+  
+}
+
+//using for of on String
+let greeting  = "Danish Hussain !"
+for (const greet of greeting) {
+  console.log(greet);
+  
+}
