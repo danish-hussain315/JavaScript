bf51547184270c2f903035204c3946d7836a19be
danish-hussain315 <danishhussain.cs.official@gmail.com>
2025-09-11 19:28:12 +0500

    Dom Concepts

---
+++ Dom-Manipulation/index.js
@@ -0,0 +1,18 @@
+// let parent = document.querySelectorAll(".day")
+ let parent = document.querySelector(".parent")
+/*console.log(parent.children);            //HTMLCollection of day day day day
+console.log(parent);
+
+for (let index = 0; index < parent.children.length; index++) {
+  console.log(parent.children[index].innerHTML);
+}
+parent.children[0].style.color = "Green" */
+
+
+const dayOne = document.querySelector(".day")
+console.log(dayOne);
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+
+
+console.log("Nodes :::: " , parent.childNodes);
