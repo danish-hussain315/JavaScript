077da846957810c0109ec8923bdc2a984cb3ad53
danish-hussain315 <danishhussain.cs.official@gmail.com>
2025-09-21 06:38:45 +0500

    Promises

---
+++ Advance JS/Promises/promise.js
@@ -0,0 +1,64 @@
+/* //Production and Consuming Seperately
+let promiseOne =  new Promise(function(resolve , reject){                 // Promise Production
+  setTimeout(function(){
+    console.log("Function Completed!");
+    resolve()
+  },1000)
+})
+
+// Promise Consuming
+promiseOne.then(function(){
+  console.log("Fuction done through Then");
+  
+})
+
+
+
+//Production and Consuming collectively
+// During Production is Not Mandatory to assign Promise to the variable
+new Promise(function(resolve , reject){                 
+    setTimeout(function(){
+    console.log("Function Completed!");
+    resolve()
+  },1000)
+}).then(function(){                // Rapid Promise Consuming after Promise Production /Creation
+  console.log("Fuction done through Then");
+}) */
+
+
+/* //Passing Value to Resolve
+let promiseThree = new Promise((resolve, reject)=>{
+  setTimeout(() =>{
+    resolve({"username" : "Danish Hussain" , id : 123})
+  },1000)
+})
+
+
+promiseThree.then((user) => {
+  console.log(user['username']);
+})
+ */
+
+
+
+
+let promiseFour = new Promise(function (resolve , reject){
+  let checkUp = true
+  if(!checkUp){
+    reject("Reject is Called")
+  }else{
+    resolve({"username" : "Danish Hussain" , id : 123})
+  }
+})
+
+promiseFour.then(function(user){            //return statement in 
+  console.log("User From FirstChain ", user);       
+  return user["username"]                   // this return cannot be returned to variable
+})                                        // It will return to the next then Chaining Concept
+.then((username) => {
+  console.log("UserName From Second Then" , username);
+  
+})
+.catch((msg)=>{
+  console.log(msg);
+})
